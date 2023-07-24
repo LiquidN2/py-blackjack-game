4d4cd83fe52ebedc23eea603f6495ef0607084aa
Hugh Nguyen <hhnguyen255@gmail.com>
2023-07-24 17:55:49 +0700

    updates

--- main.py
+++ main.py
@@ -29,17 +29,30 @@ if __name__ == "__main__":
 
         print(f"Your cards: [{player_me.show_cards()}]")
         print(f"Your current score is: {player_me.current_score}")
-        if player_me.is_blackjack():
-            print("Congrats! You have Blackjack!")
-
         print(f"Computer's cards: [{computer.show_cards(is_first_card_only=True)}, *]")
-        if computer.is_blackjack():
-            print("The computer has Blackjack ☹️")
 
         while True:
             another_card = input("Would you like to another card? (Answer: y/n) ").strip()
-            if another_card != 'y':
+            if another_card != 'y' or player_me.current_score > 21 or len(player_me.cards) >= 5:
                 break
+
             player_me.get_card()
             print(f"You've drawn a {player_me.show_last_card()}")
             print(f"Your current score is: {player_me.current_score}")
+
+            if computer.current_score < 21 and len(computer.cards) < 5:
+                computer.get_card()
+
+        while True:
+            if computer.current_score >= 21 or len(computer.cards) >= 5:
+                break
+            computer.get_card()
+
+        print(f"The computer score is {computer.current_score}")
+        if player_me.current_score > computer.current_score:
+            print("Congratulations! You win!")
+            continue
+
+        if player_me.current_score < computer.current_score:
+            print("Sorry, the computer wins")
+            continue
